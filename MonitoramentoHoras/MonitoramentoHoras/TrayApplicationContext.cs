03c864357d3b954062aefdacd545d091e1c7a0e3
Gus <gustavojo47@gmail.com>
2021-10-21 21:34:19 -0300

    Aplicativo rodando na tray

---
+++ MonitoramentoHoras/MonitoramentoHoras/TrayApplicationContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MonitoramentoHoras
+{
+    public class TrayApplicationContext : ApplicationContext
+    {
+        NotifyIcon notifyIcon = new NotifyIcon();
+
+        public TrayApplicationContext()
+        {
+            ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("Sair", null, new EventHandler(Exit));
+
+            notifyIcon.Icon = Properties.Resources.IconeTray;
+            notifyIcon.DoubleClick += new EventHandler(ShowMessage);
+            notifyIcon.ContextMenuStrip = new ContextMenuStrip();
+            notifyIcon.ContextMenuStrip.Items.Insert(0, exitMenuItem);
+            notifyIcon.Visible = true;
+        }
+
+        void ShowMessage(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mensagem de duplo clique");
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+
+            Application.Exit();
+        }
+    }
+}
