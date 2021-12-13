571ce94b6ee641e2fee2f37e2d5af74cac2e40c2
Gus <gustavojo47@gmail.com>
2021-12-12 22:30:02 -0300

    Ajustes desktop

--- MonitoramentoHoras/MonitoramentoHoras/TrayApplicationContext.cs
+++ MonitoramentoHoras/MonitoramentoHoras/TrayApplicationContext.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Drawing;
+using System.IO;
+using System.Net.Http;
 using System.Text;
+using System.Timers;
 using System.Windows.Forms;
 using Win32_API;
 
@@ -11,29 +14,101 @@ namespace MonitoramentoHoras
     public class TrayApplicationContext : ApplicationContext
     {
         NotifyIcon notifyIcon = new NotifyIcon();
+        bool pontoAtivo = false;
+        private static System.Timers.Timer timer;
+        private DateTime? inicioAusencia = null;
+        private static readonly HttpClient client = new HttpClient();
 
         public TrayApplicationContext()
         {
-            ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("Sair", null, new EventHandler(Exit));
+            ToolStripMenuItem iniciarMenuItem = new ToolStripMenuItem("Iniciar ponto", null, new EventHandler(IniciarPonto));
+            ToolStripMenuItem sairMenuItem = new ToolStripMenuItem("Sair", null, new EventHandler(Sair));
 
-            notifyIcon.Icon = Properties.Resources.IconeTray;
-            notifyIcon.DoubleClick += new EventHandler(ShowMessage);
+            using (MemoryStream ms = new MemoryStream(Properties.Resources.IconeTray))
+            {
+                notifyIcon.Icon = new Icon(ms);
+            }
             notifyIcon.ContextMenuStrip = new ContextMenuStrip();
-            notifyIcon.ContextMenuStrip.Items.Insert(0, exitMenuItem);
+            notifyIcon.ContextMenuStrip.Items.Insert(0, sairMenuItem);
+            notifyIcon.ContextMenuStrip.Items.Insert(1, iniciarMenuItem);
             notifyIcon.Visible = true;
+
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += new ElapsedEventHandler(EnviarIdleTime);
+            timer.Enabled = true;
         }
 
-        void ShowMessage(object sender, EventArgs e)
+        void EnviarIdleTime(object sender, EventArgs e)
         {
-            Win32.GetIdleTime();
-            MessageBox.Show("Mensagem de duplo clique");
+            var idleTime = Win32.GetIdleTime();
+            if (idleTime >= 300000)
+            {
+                if (!inicioAusencia.HasValue)
+                {
+                    inicioAusencia = DateTime.Now.AddMinutes(-5);
+                }
+            }
+            else
+            {
+                if (inicioAusencia.HasValue)
+                {
+                    var values = new Dictionary<string, string>
+                    {
+                        { "tempoInicialOciosidade", inicioAusencia.ToString() },
+                        { "tempoFinalOciosidade", DateTime.Now.ToString() },
+                        { "pessoa", "0" }
+                    };
+
+                    var content = new FormUrlEncodedContent(values);
+
+                    var response = client.PostAsync("https://backend-monitoramento-horas.herokuapp.com/api/rastreamento", content).Result;
+
+                    var responseString = response.Content.ReadAsStringAsync();
+
+                    inicioAusencia = null;
+                }
+            }
         }
 
-        void Exit(object sender, EventArgs e)
+        void Sair(object sender, EventArgs e)
         {
             notifyIcon.Visible = false;
 
             Application.Exit();
+        }
+
+        void IniciarPonto(object sender, EventArgs e)
+        {
+            if (!pontoAtivo)
+            {
+                pontoAtivo = true;
+                notifyIcon.ContextMenuStrip.Items.RemoveAt(1);
+
+                ToolStripMenuItem finalizarMenuItem = new ToolStripMenuItem("Finalizar ponto", null, new EventHandler(FinalizarPonto));
+
+                notifyIcon.ContextMenuStrip.Items.Insert(1, finalizarMenuItem);
+            }
+            else
+            {
+                throw new Exception("O ponto já foi iniciado.");
+            }
+        }
+
+        void FinalizarPonto(object sender, EventArgs e)
+        {
+            if (pontoAtivo)
+            {
+                pontoAtivo = false;
+                notifyIcon.ContextMenuStrip.Items.RemoveAt(1);
+
+                ToolStripMenuItem iniciarMenuItem = new ToolStripMenuItem("Iniciar ponto", null, new EventHandler(IniciarPonto));
+
+                notifyIcon.ContextMenuStrip.Items.Insert(1, iniciarMenuItem);
+            }
+            else
+            {
+                throw new Exception("O ponto não foi iniciado.");
+            }
         }
     }
 }
