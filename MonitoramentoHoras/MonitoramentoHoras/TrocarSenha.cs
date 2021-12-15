6a2f3cad7a2920c2d01f59cdc0f53887c93fb1c8
Gus <gustavojo47@gmail.com>
2021-12-14 21:23:20 -0300

    Criação da tela de trocar senha

---
+++ MonitoramentoHoras/MonitoramentoHoras/TrocarSenha.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MonitoramentoHoras
+{
+    public partial class TrocarSenha : Form
+    {
+        public TrocarSenha()
+        {
+            InitializeComponent();
+        }
+    }
+}
